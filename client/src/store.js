18ea23e2d0f5594ace6522c99ac01c1774e65274
koechronix <koechronix@yahoo.com>
2024-04-02 17:32:10 +0300

    added curl braces on store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,10 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
+
 
 import { composeWithDevTools } from "redux-devtools-extension";
+
+
 import {
   productsReducer,
   productDetailsReducer,
